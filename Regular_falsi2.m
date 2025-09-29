4aa428a25f3702752d130977f06b52cfb81518b7
Jahidur Rahman Mahim <jahidurmahim@gmail.com>
2025-09-29 12:26:25 +0600

    Added MATLAB script for Regular Falsi Method(2) with auto-adjusted guesses

---
+++ Regular_falsi2.m
@@ -0,0 +1,49 @@
+%setting x as symbolic variable
+syms x;
+
+%Input section
+
+y=input('Enter non-linear equation: ');
+a=input('Enter first guess: ');
+b=input('Enter second guess: ');
+e=input('Tolerable error: ');
+
+%Finding functional value
+
+fa= eval(subs(y,x,a));
+fb= eval(subs(y,x,b));
+
+%implementation Bisection method
+if fa*fb> 0
+    disp('Given initial values do not bracket the root. ');
+end
+
+%update part of guessing
+while(fa*fb>=0)
+    a=a+1;
+    b=b+1;
+    fa= eval(subs(y,x,a));
+    fb= eval(subs(y,x,b));
+end
+    c= (b*fa-a*fb)/(fa-fb);    %false position method another law
+    fc= eval(subs(y,x,c));
+   fprintf('\n\na\t\t\tb\t\t\tc\t\t\tf(c)\n ');
+
+    while abs(fc)>e
+        fprintf('%f\t%f\t%f\t%f\n', a,b,c,fc);
+
+        if fa*fc<0
+            b =c;
+        else
+            a=c;
+        end
+
+        c= (b*fa-a*fb)/(fa-fb);
+        fc=eval(subs(y,x,c));
+    end
+
+ fprintf('\nRoot is: %f\n', c);
+ 
+% Regular Falsi (False Position) Method implementation to find roots of a user-defined non-linear equation with automatic guess adjustment
+
+
